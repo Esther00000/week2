4b880647954745c4a70d7b1e1de3f1ad6a803996
Esther <fuest8456258@gmail.com>
2023-01-05 19:33:19 +0800

    HW2

---
+++ login.js
@@ -0,0 +1,34 @@
+//引入vue3
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+createApp({
+    data(){
+        return {
+            // 登入的帳戶密碼 透過 v-model 雙向綁定 取的用戶輸入內容
+            user: {
+                username: '',
+                password: '',
+            }
+        }
+    },
+    methods : {
+        login() {
+            const apiUrl = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+            axios
+                .post(apiUrl,this.user)
+                //登入成功的結果
+                .then((res) => {
+                    //取得回傳資料的 token expired
+                    const { token, expired } = res.data;
+                    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+                    //頁面跳轉
+                    window.location = 'products.html';
+                })
+                //登入失敗的結果
+                .catch((error) => {
+                    alert('登入失敗請重新輸入');
+                    console.dir(error.response);
+                })
+        }
+    }
+}).mount('#app')
+
---
+++ products.js
@@ -0,0 +1,52 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'esther-hexschool',
+      products: [],  //承載API所有商品資料
+      detailProduct: {}, // 指定單一商品詳細資料
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          //登入成功時，取的商品資料
+          this.getData();
+        })
+        .catch((err) => {
+          alert('登入失敗')
+          //頁面跳轉至login.html
+          window.location = 'login.html';
+        })
+    },
+    //取得商品資料
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((response) => {
+          //response.data.products 為API商品資料
+          this.products = response.data.products;
+          console.log(this.products)
+        })
+        .catch((err) => {
+          alert('資料獲取失敗');
+        })
+    },
+    //展開商品的詳細資料
+    openProduct(item) {
+      this.detailProduct = item; 
+    }
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    //把取出的token放入headers裡一起發送請求
+	  axios.defaults.headers.common["Authorization"] = token;
+    // 確認是否登入
+    this.checkAdmin()
+  }
+}).mount('#app');
